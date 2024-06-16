1dc9a06f6d14d35e34ecead9e607e23c4ba4c32d
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2024-06-16 19:17:59 +0500

    fix deployment bug

--- client/config/index.ts
+++ client/config/index.ts
@@ -1,1 +1,2 @@
-export const baseUrl = "http://localhost:4000/api/v1";
+export const baseUrl = "https://devanice-assignment-backend.vercel.app/api/v1";
+// export const baseUrl = "http://localhost:4000/api/v1";
